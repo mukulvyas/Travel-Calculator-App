94881f913a6a49f0946f09c06073630ecd04b234
mukul <mukulvyas19@gmail.com>
2024-02-17 18:49:17 +0530

    adding functionality while clicking next station and showing list

--- app/src/main/java/com/example/travelapp/ContentScreen/ContentScreen.kt
+++ app/src/main/java/com/example/travelapp/ContentScreen/ContentScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -27,6 +29,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,6 +47,36 @@ import com.example.travelapp.MainScreen.ContentMain
 @Composable
 fun contentScreen(navController: NavController,src:String, dst:String){
 
+    val citySource = listOf(
+        "SELECT CITY",
+        "MAJLIS PARK",
+        "AZADPUR",
+        "SHALIMAR BAGH",
+        "NETAJI SUBHASH PLACE",
+        "SHAKURPUR",
+        "PUNJABI BAGH WEST",
+        "ESI-BASAIDARPUR",
+        "RAJOURI GARDEN",
+        "MAYAPURI",
+        "NARAINA VIHAR",
+        "DELHI CANTT.",
+        "DURGABAI DESHMUKH SOUTH CAMPUS",
+        "SIR M. VISHWESHWARAIAH MOTI BAGH",
+        "BHIKAJI CAMA PLACE",
+        "SAROJINI NAGAR",
+        "DILLI HAAT - INA",
+        "SOUTH EXTENSION",
+        "LAJPAT NAGAR",
+        "VINOBAPURI",
+        "ASHRAM",
+        "SARAI KALE KHAN - NIZAMUDDIN",
+        "MAYUR VIHAR - 1"
+    )
+
+    val filteredCities = citySource
+        .dropWhile { it != src }
+        .dropLastWhile { it != dst }
+        .distinct()
 
 
     Scaffold(
@@ -72,24 +108,47 @@ fun contentScreen(navController: NavController,src:String, dst:String){
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Heading
-            entryTextView()
-            //Text(text = "$src")
-            //Text(text = "$dst")
+            entryTextView(src,dst,filteredCities)
 
-        }
+
+//             Text(text = "$src")
+//            Text(text = "$dst")
+
+            }
 
     }
 
 }
+
 @Composable
-@Preview
-fun entryTextView() {
-    var getSource = "src"
-    var getDestination = "dst"
-    var currentPlace = "Delhi"
-    var eachPlaceDistance = 0
-    var totalDistanceCover = 0
-    var totalDistanceLeft = 100
+//@Preview
+fun entryTextView(src: String, dst: String ,filteredCities: List<String>) {
+//    var getSource = "src"
+//    var getDestination = "dst"
+//    var currentPlace = "Delhi"
+//    var eachPlaceDistance = 0
+//    var totalDistanceCover = 0
+//    var totalDistanceLeft = 100
+//    var currentUnit by remember { mutableStateOf(UnitType.KILOMETERS) }
+//    var conversionFactor = if (currentUnit == UnitType.KILOMETERS) 1.0 else 0.621371
+
+    var getSource = src
+    var getDestination = dst
+    var currentPlace = src
+    var eachPlaceDistanceKm = 1.0
+    var eachPlaceDistanceMiles by remember { mutableStateOf(1.60934) }
+    var totalDistanceLeftKm = 2.0
+    var totalDistanceLeftMiles by remember { mutableStateOf(1.60934) }
+    var totalDistanceCoverKm = 1.0
+    var totalDistanceCoverMiles by remember { mutableStateOf(1.60934) }
+    var showList by remember { mutableStateOf(false) }
+    var currentIndex by remember { mutableStateOf(0) }
+
+
+
+    // State variables for unit conversion
+    var currentUnit by remember { mutableStateOf(UnitType.KILOMETERS) }
+
 
     Column(
         modifier = Modifier
@@ -133,17 +192,20 @@ fun entryTextView() {
                             .padding(bottom = 8.dp)
                     )
                     OutlinedTextField(
-                        value = currentPlace,
+                        value = filteredCities.getOrNull(currentIndex) ?: "",
                         onValueChange = { },
                         label = { Text("Current") },
                         readOnly = true,
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(bottom = 8.dp)
+
                     )
                     OutlinedTextField(
-                        value = eachPlaceDistance.toString(),
-                        onValueChange = { },
+                        value = if (currentUnit == UnitType.KILOMETERS) eachPlaceDistanceKm.toString() else eachPlaceDistanceMiles.toString(),
+                        onValueChange = {
+                            // Handle value change if needed
+                        },
                         label = { Text("Distance") },
                         readOnly = true,
                         modifier = Modifier
@@ -151,13 +213,15 @@ fun entryTextView() {
                             .padding(bottom = 16.dp)
                     )
                     OutlinedTextField(
-                        value = totalDistanceLeft.toString(),
-                        onValueChange = { },
+                        value = if (currentUnit == UnitType.KILOMETERS) totalDistanceLeftKm.toString() else totalDistanceLeftMiles.toString(),
+                        onValueChange = {
+                            // Handle value change if needed
+                        },
                         label = { Text("Distance Left") },
                         readOnly = true,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(bottom = 16.dp)
+                            .padding(top = 16.dp)
                     )
                 }
 
@@ -170,15 +234,12 @@ fun entryTextView() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    Button(onClick = { },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 5.dp))
-                    {
-                        Text(text = "List Station")
-                    }
                     Button(
-                        onClick = { },
+                        onClick = {
+                            currentIndex = (currentIndex + 1) % filteredCities.size
+                            currentPlace = filteredCities[currentIndex]
+
+                        },
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(bottom = 5.dp)
@@ -186,15 +247,67 @@ fun entryTextView() {
                         Text(text = "Next Station")
                     }
                     Button(
-                        onClick = { },
+                        onClick = {
+                            // Toggle between kilometers and miles
+                            currentUnit = if (currentUnit == UnitType.KILOMETERS) {
+                                eachPlaceDistanceMiles = eachPlaceDistanceKm * 1.60934
+                                totalDistanceLeftMiles = totalDistanceLeftKm * 1.60934
+                                totalDistanceCoverMiles = totalDistanceCoverKm * 1.60934
+                                UnitType.MILES
+                            } else {
+                                eachPlaceDistanceKm = eachPlaceDistanceMiles / 1.60934
+                                totalDistanceLeftKm = totalDistanceLeftMiles / 1.60934
+                                totalDistanceCoverKm = totalDistanceCoverMiles / 1.60934
+                                UnitType.KILOMETERS
+                            }
+                        },
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(bottom = 8.dp)
                     ) {
                         Text(text = "Convert Miles/KM")
                     }
+
+                    Button(
+                        onClick = { showList = !showList },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 5.dp)
+                    ) {
+                        Text(text = "List")
+                    }
+                }
+            }
+
+        }
+        if (showList) {
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(7.dp)
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    items(filteredCities) { city ->
+                        Text(
+                            text = city,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        )
+                    }
                 }
             }
         }
+
+
     }
+}
+
+enum class UnitType {
+    KILOMETERS,
+    MILES
 }
