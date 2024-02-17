33780c8cc7765a0572ab24918a2382c7d71d5679
mukul <mukulvyas19@gmail.com>
2024-02-17 13:41:46 +0530

    creating a appropriate drop down menu and new windows for further process

--- app/src/main/java/com/example/travelapp/ContentScreen/ContentScreen.kt
+++ app/src/main/java/com/example/travelapp/ContentScreen/ContentScreen.kt
@@ -1,8 +1,12 @@
 package com.example.travelapp.ContentScreen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun contentScreen(){
+fun contentScreen(navController: NavController,src:String, dst:String){
 
+    Text(text = "$src")
+    Text(text = "$dst")
 }
--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.travelapp
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,8 +28,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,7 +35,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -46,7 +42,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
+import com.example.travelapp.navigation.cityNavigate
 import com.example.travelapp.ui.theme.TravelAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +54,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Source_Destination_Dropdown()
+
+                    cityNavigate()
+
+
                 }
             }
         }
@@ -67,109 +66,8 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Source_Destination_Dropdown(){
-
-    var selectSource by remember { mutableStateOf("") }
-    var selectDestination by remember { mutableStateOf("") }
-    val sourceStore = remember { mutableStateOf("") }
-    val destinationStore = remember { mutableStateOf("") }
-    val scrollingState = rememberScrollState()
-    val context = LocalContext.current
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Travel App") },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Blue),
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .verticalScroll(scrollingState),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // Heading
-            Text(
-                text = "Welcome to Distance Calculator",
-                fontSize = 25.sp,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.ExtraBold,
-                color = Color.Red,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-            ) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(12.dp),
-                    shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-                    colors = CardDefaults.cardColors(containerColor = Color.White),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-                ) {
-
-                    CityDropDown { source ->
-                        selectSource = source
-                    }
-
-                    CityDropDown { destination ->
-                        selectDestination = destination
-                    }
-
-                    Spacer(modifier = Modifier.size(20.dp))
-
-                    Button(
-                        onClick = {
-                            //Log.d("TAG", "sourceStore: ${selectSource}, destinationStore: ${selectDestination}")
-                            if (selectSource == selectDestination) {
-                                Log.d("TAG", "Hello")
-                                Toast.makeText(context, "Please try again. Source and destination are the same.", Toast.LENGTH_SHORT).show()
-                            } else {
-                                sourceStore.value = selectSource
-                                destinationStore.value = selectDestination
-                            }
-                        },
-                        modifier = Modifier
-                            .size(150.dp, 50.dp)
-                            .align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Submit")
-                    }
-
-
-                    Spacer(modifier = Modifier.size(50.dp))
-
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        OutlinedTextField(
-                            value = sourceStore.value,
-                            onValueChange = {  },
-                            label = { Text("Source") },
-                            readOnly = true
-                        )
-
-                        OutlinedTextField(
-                            value = destinationStore.value,
-                            onValueChange = {  },
-                            label = { Text("Destination") },
-                            readOnly = true
-                        )
-                    }
-                }
-            }
-        }
-    }
+fun Source_Destination_Dropdown(content: @Composable () -> Unit) {
+                content()
 }
 
 
@@ -178,6 +76,6 @@ fun Source_Destination_Dropdown(){
 @Composable
 fun GreetingPreview() {
     TravelAppTheme {
-        Source_Destination_Dropdown()
+        cityNavigate()
     }
 }
